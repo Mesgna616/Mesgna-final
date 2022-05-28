5876f5dad42a421add8e87023eca1f0bd2c83abf
mesgna <mesgna.mehari8@gmail.com>
2022-05-28 18:47:17 +0100

    final

--- pen-export-bGoEzvN/src/script.js
+++ pen-export-bGoEzvN/src/script.js
@@ -27,6 +27,27 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_image3.setAttribute("src", images[0]);
 
 });
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('submit').addEventListener('click', (event) => {
+  let element_comments = document.getElementById('comments');
+  let new_div = document.createElement('div');
+  new_div.innerText = getNumberOrString(document.getElementById('addcomment').value);
+
+  element_comments.appendChild(new_div);
+
+});
 const {
   core: { test, expect, run },
   prettify
